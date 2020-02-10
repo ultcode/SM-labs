8b1665710c8876f0096c1fb347828655e7c90068
ultcode <devidedsourse@protonmail.com>
2020-02-10 10:47:19 +0200

    Сделал код более универсальным

--- lab3/Newton's method.py
+++ lab3/Newton's method.py
@@ -1,34 +1,36 @@
-
 ### Перевіряємо функцію на збіжність, проміжок
-def function(x):
-    return (x**4 - 2*x - 1)
-
-def derivative1(x):
-    return ((4*(x**3)) - 2)
-
-def derivative2(x):
-    return (12*(x**2))
-
-def convengence (a, b):
-    if ((function(a)* derivative2(a)) > 0):
+def convengence (f,deriv2,a, b):
+    if ((f(a)* deriv2(a)) > 0):
         print("Умова збіжності виконується для першої границі, отже і для другої.")
         return a
-    elif ((function(b)* derivative2(b)) > 0):
+    elif ((f(b)* deriv2(b)) > 0):
         print("Умова збіжності виконуються для другої границі, отже і для першої.")
         return b
     else:
         print("Умова не виконується для жодної з границь")
-#Проводимо ітераюцію для необхідної точності
 
-def iteration(x):
-    for k in range(10):
-        f = x - (function(x)/ derivative1(x))
-    if (abs(function(f)) < 0.0001):
-        print(f)
-    else: iteration(f)
+#Проводимо ітераюцію для необхідної точності
+def iteration(function,derivative1, x,eps):
+    f = x - (function(x)/ derivative1(x))
+    if (abs(function(f)) < eps):
+        return f
+    else: 
+        return iteration(function,derivative1, f,eps)
 
 
 if __name__ == "__main__":
+    def function(x):
+        return (x**4 - 2*x - 1)
+
+    def derivative1(x):
+        return ((4*(x**3)) - 2)
+
+    def derivative2(x):
+        return (12*(x**2))
+
     a = float(input("Введіть значення початку границі:"))
     b = float(input("Введіть значення кінця границі:"))
-    iteration(convengence(a,b))
+    eps = float(input("Введіть значення похибки:"))
+
+    conv = convengence(function,derivative2,a,b)
+    print(iteration(function,derivative1,conv,eps))
