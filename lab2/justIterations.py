69858c52f89570bedf4a2ee89b505620b680ca41
ultcode <devidedsourse@protonmail.com>
2020-02-10 10:21:23 +0200

    Сделал чуть более читаемым

--- lab2/justIterations.py
+++ lab2/justIterations.py
@@ -1,4 +1,4 @@
-import math as m
+import math
 
 """TASK 3
 Write function in def funcFrom
@@ -7,44 +7,42 @@ And program find the root of equation with accuracy which you entered"""
 
 def multiplying_less_than_zero(par1, par2):
     result = par1 * par2
-    if result < 0:
-        return True
-    return False
-
+    return result < 0
 
 def middleMean(x1, x2):
     return (x1 + x2) / 2
 
-
 def apprMean(x1, x2):
-    return m.fabs(x1 - x2) / 2
+    return math.fabs(x1 - x2) / 2
 
-
-def searchOnInterval(x1, x2):
-    f1 = funcFrom(x1)
+def searchOnInterval(funct,epsilon,x1, x2):
+    f1 = funct(x1)
     appr = apprMean(x1, x2)
     middle_x = middleMean(x1, x2)
-    iterator = 0
 
+    iteration = 0
     while appr > epsilon:
         store = middle_x
-        if multiplying_less_than_zero(f1, funcFrom(store)):
+        if multiplying_less_than_zero(f1, funct(store)):
             x2 = store
         else:
             x1 = store
-        iterator += 1
+        iteration += 1
         middle_x = middleMean(x1, x2)
         appr = apprMean(x1, x2) / 2
-        print(f"{iterator:}.F(X) = {funcFrom(middle_x):-9};  \t{('X =' + str(middle_x)).rjust(30)}")
-        # print(f"{iterator:}.F(X) = {funcFrom(middle_x):-9};\nX = {str(middle_x)}", end='\n\n')
+
+        print(f"{iteration:}. F(X) = {funct(middle_x):-9};  \t{('X =' + str(middle_x)).rjust(30)}")
     return middle_x
 
 
 if __name__ == "__main__":
     def funcFrom(x):
         return x ** 4 - 2 * x - 1
+
     print("""Please, enter only the float nums!""")
     _from_ = float(input("Start of interval: "))
     _to_ = float(input("End of interval: "))
     epsilon = float(input("Accuracy: "))
-    print("The root of the equation: ", searchOnInterval(_from_, _to_), ". With accuracy: ", epsilon)
+
+    print("The root of the equation: ", searchOnInterval(funcFrom,epsilon,_from_, _to_),
+          ". With accuracy: ", epsilon)
