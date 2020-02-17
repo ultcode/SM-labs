167b37add45ac153c031527c450dbba3f8b08ba8
ultcode <devidedsourse@protonmail.com>
2020-02-17 03:45:12 +0200

    Добавил счетчик

--- lab3/Newton's method.py
+++ lab3/Newton's method.py
@@ -10,12 +10,13 @@ def convengence (f,deriv2,a, b):
         print("Умова не виконується для жодної з границь")
 
 #Проводимо ітераюцію для необхідної точності
-def iteration(function,derivative1, x,eps):
+def iteration(function,derivative1, x,eps,iter_numb):
     f = x - (function(x)/ derivative1(x))
+    print(str(iter_numb) + ". " + str(f))
     if (abs(function(f)) < eps):
         return f
     else: 
-        return iteration(function,derivative1, f,eps)
+        return iteration(function,derivative1, f,eps,iter_numb + 1)
 
 
 if __name__ == "__main__":
@@ -33,4 +34,4 @@ if __name__ == "__main__":
     eps = float(input("Введіть значення похибки:"))
 
     conv = convengence(function,derivative2,a,b)
-    print(iteration(function,derivative1,conv,eps))
+    print(iteration(function,derivative1,conv,eps,0))
