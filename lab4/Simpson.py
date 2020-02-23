ce179fe0b9b8369bc45a8159729d46c9399aa20c
KorolSt <korolst43@gmail.com>
2020-02-23 23:44:51 +0200

    Sympson method

---
+++ lab4/Simpson.py
@@ -0,0 +1,47 @@
+
+def funct(x):
+    return ((x**2)/(625 - x**4))
+
+
+def calc(funct,a,b,step):
+  result = 0;
+  m = (a - b) / (step*2)
+
+  x_left = a + step
+  i_counter = 1;
+  while (x_left != b):
+    if i_counter % 2 != 0:
+      result += 4 * funct(x_left)
+    x_left += step
+    i_counter += 1
+
+  x_left = a + step
+  i_counter = 1;
+  while (x_left != b - step):
+    if i_counter % 2 == 0:
+      result += 2 * funct(x_left)
+    x_left += step
+    i_counter += 1
+
+
+  result += funct(a) + funct(b)
+  result *= step/3
+  return result
+
+
+
+if __name__=="__main__":
+
+    m= 0
+    b = 4
+    step1 = 1.0
+    step2 = 0.5
+    lastX = 4
+
+
+    print ("Результат з кроком 1:", calc(funct,m,b,step2))
+
+    print("Результат з кроком 0.5:", calc(funct,m,b,step1))
+
+
+
